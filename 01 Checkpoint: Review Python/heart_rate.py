50f6a4ac688636b34ed1f023ed4d93120da02c2b
joshuaadu <joshuaadu2001@gmail.com>
2023-04-21 10:33:49 +0000

    complete week 1 assignment

---
+++ 01 Checkpoint: Review Python/heart_rate.py
@@ -0,0 +1,27 @@
+from math import floor
+
+
+def heart_rate_calculator():
+    """
+    When you physically exercise to strengthen your heart, you
+    should maintain your heart rate within a range for at least 20
+    minutes. To find that range, subtract your age from 220. This
+    difference is your maximum heart rate per minute. Your heart
+    simply will not beat faster than this maximum (220 - age).
+    When exercising to strengthen your heart, you should keep your
+    heart rate between 65% and 85% of your heart’s maximum rate.
+    """
+
+    age = int(input("Please enter your age: "))
+    heart_rate = 220 - age
+    max_heart_rate = floor(heart_rate * 0.85)
+    min_heart_rate = floor(heart_rate * 0.65)
+    print(f"When you exercise to strengthen your heart, you should")
+    print(f"keep your heart rate between {min_heart_rate} and {max_heart_rate} beats per minute.")
+
+
+
+
+if __name__ == "__main__":
+    heart_rate_calculator()
+    # print(heart_rate_calculator.__doc__)
